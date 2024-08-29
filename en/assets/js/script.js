2757b51e776c90ce34a5dcf0dd30a82f9ec940e9
shadi <ferwana5678@gmail.com>
2024-08-29 12:26:36 +0300

    update

--- en/assets/js/script.js
+++ en/assets/js/script.js
@@ -2,6 +2,7 @@
  * WOW
  */
 
+
 if (document.querySelector(".section-3  .images")) {
     var swiper = new Swiper(".section-3  .images", {
 
@@ -44,6 +45,13 @@ if (document.querySelector(".main-slider")) {
         on: {
             slideChange: function() {
                 let index = this.activeIndex;
+                var header = document.querySelector(".lang a");
+
+                if (index > 0) {
+                    header.classList.add("scrolled");
+                } else {
+                    header.classList.remove("scrolled");
+                }
                 if (index == 4) {
                     animateCounter("counter1", 24, 2000);
                     animateCounter("counter2", 5.7, 3000);
